634c4c69728bf613a091dc37fc5fde7985151811
SeanZarzycki <Sean.F.Zarzycki@gmail.com>
2017-11-20 03:02:11 -0500

    row shifter only takes rows now

--- row_shifter.vhd
+++ row_shifter.vhd
@@ -6,25 +6,19 @@ use work.aes_pkg.all;
 
 entity row_shifter is
 	port (
-		state_matrix : in matrix;
-		shifted_matrix : out matrix
+		row : in matrix;
+        row_num : in integer;
+		shifted : out matrix
 	);
 end row_shifter;
 
 architecture beh of row_shifter is
-signal shifted_result : matrix;
+
 begin
-    process(state_matrix)
+    process(row)
     begin
-        for n in 0 to MATRIX_DIM - 1 loop
-            shifted_matrix(0, n) <= state_matrix(0, n);
+        for i in 0 to MATRIX_DIM - 1 loop
+            shifted((i - row_num + MATRIX_DIM) mod MATRIX_DIM) <= state_matrix(i);
         end loop;
-        
-        for i in 1 to MATRIX_DIM - 1 loop -- skip first row
-            for j in 0 to MATRIX_DIM - 1 loop
-                shifted_matrix(i, (j - i + MATRIX_DIM) mod MATRIX_DIM) <= state_matrix(i, j);
-            end loop;
-        end loop;
-        
     end process;
 end beh;
