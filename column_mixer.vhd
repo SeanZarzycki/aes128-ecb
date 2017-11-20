5d9f9ec87651a371ebbb6d4050aaf2670f7a1595
SeanZarzycki <Sean.F.Zarzycki@gmail.com>
2017-11-20 02:05:16 -0500

    column_mixer changed to use input/output columns

--- column_mixer.vhd
+++ column_mixer.vhd
@@ -6,8 +6,8 @@ use work.aes_pkg.all;
 
 entity column_mixer is
 	port (
-		state_matrix  : in matrix;
-		mixed_columns : out matrix
+		col  : in word;
+		mixed : out word
 	);
 end column_mixer;
 
@@ -15,35 +15,33 @@ architecture beh of column_mixer is
 
 begin
     
-    process(state_matrix)
+    process(col)
 
     begin
-        for j in 0 to MATRIX_DIM - 1 loop
 
-            mixed_columns(0, j) <= state_matrix(0, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(0, j)(7)) and x"1b") xor
-                                   state_matrix(3, j) xor
-                                   state_matrix(2, j) xor
-                                   state_matrix(1, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(1, j)(7)) and x"1b") xor
-                                   state_matrix(1, j);
-            
-            mixed_columns(1, j) <= state_matrix(1, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(1, j)(7)) and x"1b") xor
-                        state_matrix(0, j) xor
-                        state_matrix(3, j) xor
-                        state_matrix(2, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(2, j)(7)) and x"1b") xor
-                        state_matrix(2, j);
-            
-            mixed_columns(2, j) <= state_matrix(2, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(2, j)(7)) and x"1b") xor
-                        state_matrix(1, j) xor
-                        state_matrix(0, j) xor
-                        state_matrix(3, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(3, j)(7)) and x"1b") xor
-                        state_matrix(3, j);
+        mixed(0) <= col(0)(6 downto 0) & "0" xor ((7 downto 0 => col(0)(7)) and x"1b") xor
+                                col(3) xor
+                                col(2) xor
+                                col(1)(6 downto 0) & "0" xor ((7 downto 0 => col(1)(7)) and x"1b") xor
+                                col(1);
+        
+        mixed(1) <= col(1)(6 downto 0) & "0" xor ((7 downto 0 => col(1)(7)) and x"1b") xor
+                    col(0) xor
+                    col(3) xor
+                    col(2)(6 downto 0) & "0" xor ((7 downto 0 => col(2)(7)) and x"1b") xor
+                    col(2);
+        
+        mixed(2) <= col(2)(6 downto 0) & "0" xor ((7 downto 0 => col(2)(7)) and x"1b") xor
+                    col(1) xor
+                    col(0) xor
+                    col(3)(6 downto 0) & "0" xor ((7 downto 0 => col(3)(7)) and x"1b") xor
+                    col(3);
 
-            mixed_columns(3, j) <= state_matrix(3, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(3, j)(7)) and x"1b") xor
-                        state_matrix(2, j) xor
-                        state_matrix(1, j) xor
-                        state_matrix(0, j)(6 downto 0) & "0" xor ((7 downto 0 => state_matrix(0, j)(7)) and x"1b") xor
-                        state_matrix(0, j);          
-        end loop;
+        mixed(3) <= col(3)(6 downto 0) & "0" xor ((7 downto 0 => col(3)(7)) and x"1b") xor
+                    col(2) xor
+                    col(1) xor
+                    col(0)(6 downto 0) & "0" xor ((7 downto 0 => col(0)(7)) and x"1b") xor
+                    col(0);          
         
     end process;
 end beh;
